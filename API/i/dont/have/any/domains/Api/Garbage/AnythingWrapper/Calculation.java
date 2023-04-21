43a7d03d4fc53d57e51b5cc199fda94ec30f1c8b
Rell-Yuna <114401012+Rell-Yuna@users.noreply.github.com>
2023-04-21 21:05:34 +0900

    Delete API/i/dont/have/any/domains/Api/Garbage directory

--- API/i/dont/have/any/domains/Api/Garbage/AnythingWrapper/Calculation.java
+++
@@ -1,7 +0,0 @@
-package i.dont.have.any.domains.Api.Garbage.AnythingWrapper;
-// package i.dont.have.any.domains.Api.AnythingWrapper;
-
-// @FunctionalInterface
-// interface Calculation<T> {
-//     T calc(T obj1 , T obj2);
-// }
